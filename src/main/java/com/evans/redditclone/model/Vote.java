1120881cddbb7ec4b832293ee86c1963ec98ffe6
Evans Shango <lutatwaevans@gmail.com>
2019-12-22 19:23:27 +0300

    implement Spring security using the SecurityConfig class

--- src/main/java/com/evans/redditclone/model/Vote.java
+++ src/main/java/com/evans/redditclone/model/Vote.java
@@ -1,4 +1,26 @@
 package com.evans.redditclone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 }
